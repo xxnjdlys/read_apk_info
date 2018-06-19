2338dbe05ddbd4beae530595a18df49a03724d56
xxnjdlys <xxnjdlys@gmail.com>
2018-06-19 17:18:38 +0800

    add readme.md for pkginfo.sh

---
+++ README.md
@@ -0,0 +1,6 @@
+### usages:
+
+- clone this repo to your local dir.
+- cd /your/local/dir 
+- chmod +x ./pkginfo.sh
+- ./pkginfo.sh  /your/apk/path
--- pkginfo.sh
+++ pkginfo.sh
@@ -3,11 +3,11 @@
 
 LYS_APK_FILE=$1
 
-LYS_PACKAGE=`aapt dump badging ${LYS_APK_FILE} | grep package | awk '{print $2}' | sed s/name=//g | sed s/\'//g`
-LYS_VERSIONCODE=`aapt dump badging ${LYS_APK_FILE} | grep versionCode | awk '{print $3}' | sed s/versionCode=//g | sed s/\'//g`
-LYS_VERSIONNAME=`aapt dump badging ${LYS_APK_FILE} | grep versionName | awk '{print $4}' | sed s/versionName=//g | sed s/\'//g`
-LYS_MINSDKVERSION=`aapt dump badging ${LYS_APK_FILE} | grep sdkVersion | sed s/sdkVersion://g | sed s/\'//g`
-LYS_TARGETSDKVERSION=`aapt dump badging ${LYS_APK_FILE} | grep targetSdkVersion: | sed s/targetSdkVersion://g | sed s/\'//g`
+LYS_PACKAGE=`./aapt dump badging ${LYS_APK_FILE} | grep package | awk '{print $2}' | sed s/name=//g | sed s/\'//g`
+LYS_VERSIONCODE=`./aapt dump badging ${LYS_APK_FILE} | grep versionCode | awk '{print $3}' | sed s/versionCode=//g | sed s/\'//g`
+LYS_VERSIONNAME=`./aapt dump badging ${LYS_APK_FILE} | grep versionName | awk '{print $4}' | sed s/versionName=//g | sed s/\'//g`
+LYS_MINSDKVERSION=`./aapt dump badging ${LYS_APK_FILE} | grep sdkVersion | sed s/sdkVersion://g | sed s/\'//g`
+LYS_TARGETSDKVERSION=`./aapt dump badging ${LYS_APK_FILE} | grep targetSdkVersion: | sed s/targetSdkVersion://g | sed s/\'//g`
 
 echo
 echo '======================================================='
